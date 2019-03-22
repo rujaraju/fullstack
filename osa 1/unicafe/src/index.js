2803ffb5c50a72cd083035b6448b34c683400d51
rujaraju <jan.rundt@gmail.com>
2019-03-22 12:08:15 +0000

    unicafe aika pitkälle

--- osa 1/unicafe/src/index.js
+++ osa 1/unicafe/src/index.js
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const Nappi = ({onclick, label}) => {
+    return (
+        <button onClick={onclick}>{label}</button>
+        )
+}
+
+const Tilasto = ({label, value}) => {
+        return(
+    <p>{label}: {value}</p>
+  )
+}
+
+const Statistics = ({values}) => {
+    if (values[3].arvo == 0) {
+        return (
+            <p>Ei yhtään palautetta annettu!!</p>
+            )
+    }
+    return values.map(elem => <Tilasto key ={elem.label} label= {elem.label} value= {elem.arvo}  />);
+}
+
+const App = () => {
+    const [hyva, setHyva] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [huono, setHuono] = useState(0)
+    const [yhteensa, setYhteensa] = useState(0)
+    const [keskiarvo, setKeskiarvo] = useState(0)
+    const [positiivisia, setPositiivisia] = useState(0)
+    var arvot = [
+        {label:"Hyvä", arvo: hyva}, 
+        {label:"Huono", arvo: huono}, 
+        {label:"Neutraali", arvo: neutral}, 
+        {label: "Yhteensä", arvo: yhteensa},
+        {label:"Keskiarvo", arvo: keskiarvo},
+        {label:"Positiivisia", arvo: positiivisia}
+        ];
+    
+    const handleClick = (value) => {
+        if (value === "hyvä"){
+            setHyva(hyva + 1);
+            setKeskiarvo((((hyva+1)*1 + huono*-1)/(yhteensa+1)).toFixed(2));
+            setPositiivisia(((hyva+1) / (yhteensa+1)).toFixed(1));
+        } else if (value === "huono"){
+            setHuono(huono + 1);
+            setKeskiarvo(((hyva*1 + (huono+1)*-1)/(yhteensa+1)).toFixed(2));
+            setPositiivisia((hyva / (yhteensa+1)).toFixed(1));
+        } else {
+            setNeutral(neutral + 1);
+            setKeskiarvo(((hyva*1 + huono*-1)/(yhteensa+1)).toFixed(2));
+            setPositiivisia((hyva / (yhteensa+1)).toFixed(1));
+        }
+        setYhteensa(yhteensa+1);
+        
+    }
+
+
+
+  return (
+    <div>
+    <h1>Mitäs mieltä olit meistä tänään?</h1>
+    <Nappi label="Hyvä" onclick={() => handleClick("hyvä")} />
+    <Nappi label="Neutraali" onclick={() => handleClick("")} />
+    <Nappi label="Huono" onclick={() => handleClick("huono")} />
+    <h3>Tulokset:</h3>
+    <Statistics values={arvot} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
